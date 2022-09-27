31888c688d6e1bf2f4fb13399ab8e85c4903c9f9
DenysKhm <106743435+DenysKhm@users.noreply.github.com>
2022-09-27 13:27:04 +0300

    fixed

--- src/pages/Cast/Cast.jsx
+++ src/pages/Cast/Cast.jsx
@@ -8,9 +8,10 @@ export const Cast = () => {
       {cast && (
         <>
           <div>
-            {cast.cast.map(({ id, name, character }) => {
+            {cast.cast.map(({ id, name, character, profile_path }) => {
               return (
                 <li key={id}>
+                    <img src={`https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${profile_path}`} alt={name} width='100'/>
                   <p>{name}</p>
                   <p>{character}</p>
                 </li>
