9afa4e4328436343ccd1f6075fbc61bf35ae0251
DenysKhm <106743435+DenysKhm@users.noreply.github.com>
2022-09-27 23:31:47 +0000

    Deploying to gh-pages from @ DenysKhm/goit-react-hw-05-movies@8117d9becef189ec01ddc828a8c121ef23d67bdc 🚀

---
+++ static/js/803.63af1811.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[803],{7803:function(e,t,o){o.r(t);var n=o(6731),c=o(184);t.default=function(){return(0,c.jsxs)("h1",{children:["Page not found. Weclome to ",(0,c.jsx)(n.rU,{to:"/",children:"home page"})]})}}}]);
+//# sourceMappingURL=803.63af1811.chunk.js.map
